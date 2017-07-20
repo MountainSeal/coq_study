99b0e25d5b17df2dd0fbe403d33f5f63076360d6
MountainSeal <yamazarashi555@gmail.com>
2017-07-20 22:41:12 +0900

    day型を書き忘れていたので追加

--- Basics.v
+++ Basics.v
@@ -1,3 +1,24 @@
+Inductive day : Type :=
+| monday : day
+| tuesday : day
+| wednesday : day
+| thursday : day
+| friday : day
+| saturday : day
+| sunday : day.
+
+Definition next_day (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
+
+
 Inductive bool : Type :=
 |true : bool
 |false : bool.
